54cf31b252790e2870f1a128312b3745d42fd9ba
JenisheSalem <jenishedufsalem@gmail.com>
2024-11-04 01:41:09 +0800

    updating navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -5,7 +5,7 @@
   <Navbar class="bg-yellow-950 text-white">
     <NavBrand href="/">
       <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Jenishe Salem</span>
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Jen's Corner</span>
     </NavBrand>
     <div class="flex items-center md:order-2">
       <Avatar id="avatar-menu" src="assets/pictures/jenishe.jpg" />
@@ -15,16 +15,15 @@
         <span class="block text-sm">Jenishe Salem</span>
         <span class="block truncate text-sm font-medium">jenishedufsalem@gmail.com</span>
       </DropdownHeader>
-      <DropdownItem>Dashboard</DropdownItem>
-      <DropdownItem>Settings</DropdownItem>
-      <DropdownItem>Earnings</DropdownItem>
-      <DropdownDivider />
-      <DropdownItem>Sign out</DropdownItem>
+      <DropdownItem>Blog</DropdownItem>
+      <DropdownItem>Video Presentation</DropdownItem>
+      <DropdownItem>Posters</DropdownItem>
+
     </Dropdown>
     <NavUl>
+      <NavLi class="text-white" href="">Home</NavLi>
+      <NavLi class="text-white" href="">Portfolio</NavLi>
       <NavLi class="text-white" href="">About</NavLi>
-      <NavLi class="text-white" href="">Navbar</NavLi>
-      <NavLi class="text-white" href="">Pricing</NavLi>
       <NavLi class="text-white" href="">Contact</NavLi>
     </NavUl>
   </Navbar>
