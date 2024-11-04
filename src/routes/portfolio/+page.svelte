b6d68aa033b675db964e87ad1953902550f4a378
JenisheSalem <jenishedufsalem@gmail.com>
2024-11-04 23:00:16 +0800

    setting up portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -5,15 +5,9 @@
       { alt: 'istorya', src: 'assets/portfolio/3.jpg' },
       { alt: 'soul', src: 'assets/portfolio/1.jpg' },
       { alt: 'buzzer', src: 'assets/portfolio/5.png' },
-      { alt: 'ccs', src: 'assets/portfolio/2.jpg' },
-      { alt: 'charcounter', src: 'assets/portfolio/4.jpg' },
-      { alt: 'shoe', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg' },
-      { alt: 'cream', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg' },
-      { alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg' },
-      { alt: 'lamp', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg' },
-      { alt: 'toiletbag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg' },
-      { alt: 'playstation', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg' },
-      { alt: 'bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg' }
+      { alt: 'ccs', src: 'assets/portfolio/4.jpg' },
+      { alt: 'charcounter', src: 'assets/portfolio/6.png' },
+      { alt: 'pills', src: 'assets/portfolio/7.png' },
     ];
   </script>
   
@@ -30,26 +24,26 @@
     
     <div class="flex flex-row space-x-4 justify-center mt-16"> <!-- Added mt-16 for more top margin -->
         <Card class="flex-1"> <!-- Flex-1 for flexible widths -->
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Video Projects</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are my video projects with voiceover and a video for an advertisement.</p>
             <Button class="w-fit">
-                Read more
+                Watch
             </Button>
         </Card>
     
         <Card class="flex-1">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Blogs</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Sometimes I spend time making blogs about computer science-related topics.</p>
             <Button class="w-fit">
-                Read more
+                Read
             </Button>
         </Card>
     
         <Card class="flex-1">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Certificates</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are my certificates from my different trainings.</p>
             <Button class="w-fit">
-                Read more
+                Check
             </Button>
         </Card>
     </div>
